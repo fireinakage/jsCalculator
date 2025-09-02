5880651b88d21a8bea671f136ea1cb99ea65962c
Akaira <forest.cafee@gmail.com>
2025-09-02 10:42:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const idChoice1 = document.getElementById('')
+const idChoice1 = document.getElementById('inputBox1');
+const idChoice2 = document.getElementById('choiceBox');
+const idChoice3 = document.getElementById('inputBox2');
 let operator = "";
 let number1 = "";
 let number2 = "";
