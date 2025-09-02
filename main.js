d788afe2383a57ba18929349a740d6bdd4354990
Akaira <forest.cafee@gmail.com>
2025-09-02 10:26:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 let operator = "";
 let number1 = "";
+let number2 = "";
+
+function calculateResult(firstNumber, symbol, secondNumber) {
+  operator = symbol;
+  number1 = firstNumber;
+  number2 = secondNumber;
+}
